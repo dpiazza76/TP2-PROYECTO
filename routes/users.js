f62103ab51f4ef9202c3dbb5f31fa79e662a6b14
ggimenez <ggimenez@tecnoaccion.com.ar>
2021-11-08 09:28:06 -0300

    Completed routes urls

--- routes/users.js
+++ routes/users.js
@@ -16,10 +16,49 @@ router.get("/", async function (req, res, next) {
   res.send(users);
 });
 
-//buscar usuario por e-mail
+//Search user by email.
 router.get("/search", async (req, res, next) => {
   const user = await getUserByEmail(req.query.email);
   res.send(user);
 });
+
+//Add user
+router.post('/', async (req, res, next) => {
+  
+  // Chequing non existance of user
+  if( await getUserByEmail(req.body.email))
+    res.json({error: "Email ya existente"});
+  
+  const user = req.body;
+  const result = await addUser(user);
+
+  res.json({"status": result.statusCode, "user":user });
+})
+
+//Update user
+router.post('/update/:id', async (req, res, next) => {
+  
+  //Chequing existance of user
+  const user = getUserById(req.params.id);
+
+  if(!user)
+    res.json({"error":"Usuario no encontrado"});
+
+  const result = updateUser(req.body);
+
+  res.json({"status": result.statusCode, "updatedProperties":req.body})
+})
+
+//Search user by id.
+router.get('/search', async (req, res, next) => {
+  const user = await getUserById(req.query.id);
+  res.send(user);
+})
+
+//Delete user by id.
+router.delete('/delete/:id', async (req, res, next) => {
+  const result = await deleteUser(req.params.id);
+  res.json({"status": res.statusCode , "result":result});
+})
 
 export default router;
